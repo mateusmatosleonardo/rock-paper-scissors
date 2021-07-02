e3d6e2d0a6bf0dab7995ba6b0041732a2266788e
mateusmatosleonardo <m4teusmatos@gmail.com>
2021-07-02 12:36:13 -0300

    js: adding description

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var elementos = document.querySelectorAll('.player-options div > img');
 
     for(var i = 0; i < elementos.length; i++){
+        /* adicionando parameters t para chamar target pq é uma função CallBack */
         elementos[i].addEventListener('click',(t)=>{
             t.target.style.opacity = 1;
         });
