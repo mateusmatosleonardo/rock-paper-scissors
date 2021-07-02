57fadfaadf0e5d5048e6b29063a09eea94fdb82f
mateusmatosleonardo <m4teusmatos@gmail.com>
2021-07-02 12:34:03 -0300

    js: adding parameters t to retrieve value

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var elementos = document.querySelectorAll('.player-options div > img');
 
     for(var i = 0; i < elementos.length; i++){
-        elementos[i].addEventListener('click',()=>{
-            alert('clicado');
-        })
+        elementos[i].addEventListener('click',(t)=>{
+            t.target.style.opacity = 1;
+        });
     }
