6362fdfccca21177828d3b12c6611dedf5aba641
mateusmatosleonardo <m4teusmatos@gmail.com>
2021-07-02 12:30:25 -0300

    js: testing aplication working

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@ var elementos = document.querySelectorAll('.player-options div > img');
 
     for(var i = 0; i < elementos.length; i++){
         elementos[i].addEventListener('click',()=>{
-            
+            alert('clicado');
         })
     }
