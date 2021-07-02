c115588e06260d28ae9bb098ce4078436fc8509c
mateusmatosleonardo <m4teusmatos@gmail.com>
2021-07-02 12:21:25 -0300

    js: adding event of click

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 var elementos = document.querySelectorAll('.player-options div > img');
+
+    for(var i = 0; i < elementos.length; i++){
+        elementos[i].addEventListener('click',()=>{
+            
+        })
+    }
