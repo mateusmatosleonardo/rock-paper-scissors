2370b26158054d510624e441e1e46b8a344cb360
mateusmatosleonardo <m4teusmatos@gmail.com>
2021-07-02 12:47:55 -0300

    js: adding function of reset

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 var elementos = document.querySelectorAll('.player-options div > img');
 
+    /* chamando função de reset */
+    function resetOpacityPlayer(){
+        for(var i = 0; i < elementos.length; i++){
+            elementos[i].style.opacity = 0.4;
+        }
+    }
+
     for(var i = 0; i < elementos.length; i++){
         /* adicionando parameters t para chamar target pq é uma função CallBack */
         elementos[i].addEventListener('click',(t)=>{
+            /* função para resetar opacidade */
+            resetOpacityPlayer();
             t.target.style.opacity = 1;
         });
     }
